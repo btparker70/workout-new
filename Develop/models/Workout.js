2423a25fe0cd19918c4eb9ca7288decccd10de82
Brian Parker <btparker70@gmail.com>
2021-02-08 03:29:57 -0800

    adding api-routes. moved from server

--- Develop/models/Workout.js
+++ Develop/models/Workout.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 // name, type, weight, sets, reps, and duration
 const WorkoutSchema = new Schema({
-  name: String,
   type: String,
+  name: String,
+  weight: Number,
   sets: Number,
   reps: Number,
   duration: Number
--- Develop/server.js
+++ Develop/server.js
@@ -18,58 +18,9 @@ app.use(express.static(path.join(__dirname + "/public")));
 
 // Routes
 app.use(require("./routes/html-routes.js"));
+app.use(require("./routes/api-routes.js"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", { useNewUrlParser: true });
-
-db.User.create({ name: "Jimmy Dunkis" })
-  .then(dbUser => {
-    console.log(dbUser);
-  })
-  .catch(({ message }) => {
-    console.log(message);
-  });
-
-app.get("/workouts", (req, res) => {
-  db.Workout.find({})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-app.get("/user", (req, res) => {
-  db.User.find({})
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-app.post("/submit", ({ body }, res) => {
-  db.Workout.create(body)
-    .then(({ _id }) => db.User.findOneAndUpdate({}, { $push: { workouts: _id } }, { new: true }))
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-app.get("/populateduser", (req, res) => {
-  db.User.find({})
-    .populate("workouts")
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
