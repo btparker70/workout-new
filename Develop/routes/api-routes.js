96b6aa1f9c688b0f64298dea2e3d635cd0245e66
Brian Parker <btparker70@gmail.com>
2021-02-09 00:19:18 -0800

    fixed api/workout needed [] for aggregate.

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -1,13 +1,17 @@
 const db = require("../models");
 const router = require("express").Router();
+var mongoose = require('mongoose');
+var ObjectId = mongoose.Types.ObjectId();
+const mongojs = require("mongojs");
 
+// Get all workouts
 router.get("/api/workouts", (req, res) => {
-db.Workout.aggregate({
+db.Workout.aggregate([{
   $addFields: {
     totalDuration: { 
       $sum: "$exercises.duration" }
   }
-})
+}])
   .then(dbWorkout => {
     res.json(dbWorkout);
   })
@@ -16,12 +20,23 @@ db.Workout.aggregate({
   });
 });
 
+// Create new workout
 router.post("/api/workouts", (req, res) => {
 db.Workout.create({})
-  .then((data) => 
-    res.json(data))
+  .then((dbWorkout) => 
+    res.json(dbWorkout))
   .catch(err => {
     console.log(err);
+    res.json(err);
+  });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  db.Workout.findOneAndUpdate({_id: mongojs.ObjectId(req.params.id)}, { $push: { exercises: req.body } }, { new: true })
+  .then(dbWorkout => {
+    res.json(dbWorkout);
+  })
+  .catch(err => {
     res.json(err);
   });
 });
