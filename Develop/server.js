952ad1f5536a1cb59906bd29dba984ddb5a57f7b
Brian Parker <btparker70@gmail.com>
2021-02-09 23:58:32 -0800

    deploying

--- Develop/server.js
+++ Develop/server.js
@@ -20,7 +20,17 @@ app.use(express.static(path.join(__dirname + "/public")));
 app.use(require("./routes/html-routes.js"));
 app.use(require("./routes/api-routes.js"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
