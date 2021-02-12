398791537266fd143723ceaf2a5734999618f6f0
Brian Parker <btparker70@gmail.com>
2021-02-11 22:46:09 -0800

    shane help

--- models/Workout.js
+++ models/Workout.js
@@ -3,37 +3,37 @@ const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
   day: {
-    type: Date, 
-    default: Date.now
+    type: Date,
+    default: Date.now,
   },
   exercises: [
     {
-    type: {
-      type: String,
-      required: "Workout type required",
+      type: {
+        type: String,
+        required: "Workout type required",
+      },
+      name: {
+        type: String,
+        required: "Workout name required",
+      },
+      duration: {
+        type: Number,
+        required: "Workout duration required",
+      },
+      weight: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
     },
-    name: {
-      type: String,
-      required: "Workout name required",
-    },
-    duration: {
-      type: Number,
-      required: "Workout duration required",
-    },
-    weight: {
-      type: Number,
-    },
-    reps: {
-      type: Number,
-    },
-    sets: {
-      type: Number,
-    },
-    distance: {
-      type: Number,
-    },
-  }
-]
+  ],
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
--- server.js
+++ server.js
@@ -2,41 +2,32 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
-
 const PORT = process.env.PORT || 3000;
-
 //const db = require("./models");
-
 const app = express();
-
 app.use(logger("dev"));
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname + "/public")));
-
 // Routes
 app.use(require("./routes/api-routes.js"));
 app.use(require("./routes/html-routes.js"));
-
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
-
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workout',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-).then(()=>{
-  console.log("Connected to the Database");
-})
-.catch(err => {
-  console.log(err);
-});
-
+mongodb+srv://webmasterShane112:Shaylyn112@cluster0.larjt.mongodb.net/workout?retryWrites=true&w=majority
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+// mongoose.connect(
+//   process.env.MONGODB_URI || 'mongodb://localhost/workout',
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false
+//   }
+// ).then(()=>{
+//   console.log("Connected to the Database");
+// })
+// .catch(err => {
+//   console.log(err);
+// });
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
