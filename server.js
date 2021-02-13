7af962e5879130a4ded291c3a1a4ecaa1898fc95
Brian Parker <btparker70@gmail.com>
2021-02-13 15:27:35 -0800

    test

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const path = require("path");
+
 const PORT = process.env.PORT || 3000;
-//const db = require("./models");
+
 const app = express();
+
 app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static(path.join(__dirname + "/public")));
+
 // Routes
 app.use(require("./routes/api-routes.js"));
 app.use(require("./routes/html-routes.js"));
