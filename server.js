1170fa030ed7650d389a1a44281fe17feb41dc01
Brian Parker <btparker70@gmail.com>
2021-02-13 15:00:01 -0800

    server

--- server.js
+++ server.js
@@ -13,16 +13,16 @@ app.use(express.static(path.join(__dirname + "/public")));
 app.use(require("./routes/api-routes.js"));
 app.use(require("./routes/html-routes.js"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
-// mongoose.connect(
-//   process.env.MONGODB_URI || 'mongodb://localhost/workout',
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false
-//   }
-// )
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+)
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
